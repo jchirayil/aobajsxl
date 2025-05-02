a0d6d0cb817617319077658d9288a5830d094983
Neha Madan <neha.madan@grapecity.com>
2025-05-02 17:27:22 +0530

    Added test for incorrect formula

--- test/base/excel-base.test.ts
+++ test/base/excel-base.test.ts
@@ -119,4 +119,18 @@ describe('Excel', () => {
     expect(fs.existsSync(filePath)).to.be.true;
     fs.unlinkSync(filePath);
   });
+
+  it('should write erroneous formula as is', async () => {
+    const filePath = path.join(__dirname, '../data/test-error-formula.xlsx'); // Construct the file path
+    excelBase['setData']('FormulaSheet', [
+        {id: 1, operation: 'multiplication', total: '=*3'},
+        {id: 2, operation: 'addition', total: '=5+2+4'}, 
+        {id: 3, operation: 'subtraction', total: '=152-24'},
+        {id: 4, operation: 'division', total: '=100/5'},
+        {id: 5, operation: 'concatenate', total: '=CONCATENATE("Hello", " ", "World")'}
+    ]);
+    await excelBase.write(filePath);
+    expect(fs.existsSync(filePath)).to.be.true;
+    // fs.unlinkSync(filePath);
+  });
 });
